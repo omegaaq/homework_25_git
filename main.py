be97b4d1f08e84aab9e228c9dc98f9d8217958e3
Natalia Oner <nataliaoner@gmail.com>
2023-03-10 14:36:26 +0300

    create menu branch and while

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('hello')
+
+i = 0
+while i != 4:
+    print('1 - задати число\n2 - змінити\n3 - занулити\n4 - вийти з меню')
+    user_input = int(input('enter number of your chice: '))
+    if user_input == 4:
+        print('bye')
+        break
+    else:
+        if user_input == 1:
+            print('1')
+        elif user_input == 2:
+            print('2')
+        elif user_input == 3:
+            print('3')
